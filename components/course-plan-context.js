708840c7b99a4ec39cc0752cd798b61d87033aac
Nils Lundquist <nils@bitovi.com>
2020-01-11 20:07:58 -0700

    fix typo

--- components/course-plan-context.js
+++ components/course-plan-context.js
@@ -4,8 +4,8 @@ import { component, useState } from 'haunted';
 import useFetch from '../helpers/use-fetch.js';
 import getComponentStyleSheetURL from '../helpers/stylesheet';
 
-  const allLessonRequest = useFetch(`${syringeServiceRoot}/exp/lesson`);
 customElements.define('antidote-course-plan-context', component(function CoursePlanContext() {
+  const allLessonRequest = useFetch(`${syringeServiceRoot}/exp/lesson`);
   const lessonPrereqRequest = useFetch(`${syringeServiceRoot}/exp/lesson/${lessonId}/prereqs`);
   const [name, setName] = useState(null);
   const [strengths, setStrengths] = useState({});
