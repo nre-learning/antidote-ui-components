6121eaf3778ffb44cf7b62e448100d030000857d
Matt Oswalt <matt@oswalt.dev>
2020-04-04 21:48:16 -0700

    Standardize lesson guide header
    Signed-off-by: Matt Oswalt <matt@oswalt.dev>

--- components/lab-guide.js
+++ components/lab-guide.js
@@ -1,11 +1,13 @@
 import { html } from 'lit-html';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
 import { useContext, useEffect, component } from 'haunted';
-import { LiveLessonDetailsContext } from '../contexts.js';
+import { LiveLessonDetailsContext, LessonContext } from '../contexts.js';
+
 import { serviceHost, lessonSlug, lessonStage } from "../helpers/page-state.js";
 import showdown from 'showdown';
 import debounce from '../helpers/debounce.js';
 import getComponentStyleSheetURL from '../helpers/stylesheet';
+import getL8nReader from '../helpers/l8n';
 
 // this function currently needs to be global since it's explicitly referenced in guide markdown content
 // this could eventually be scoped and bound to the appropriate buttons post-render
@@ -71,6 +73,8 @@ function useSyncronizedScrolling(guide) {
 }
 
 function LabGuide() {
+  const l8n = getL8nReader(this);
+  const lessonRequest = useContext(LessonContext);
   const lessonDetailsRequest = useContext(LiveLessonDetailsContext);
   let guideContent = "";
 
@@ -87,20 +91,25 @@ function LabGuide() {
     else if (lessonDetailsRequest.data.GuideType == 'markdown') {
       const converter = new showdown.Converter();
       guideContent = html`
-        <div>
-            ${unsafeHTML(converter.makeHtml(lessonDetailsRequest.data.GuideContents))}
-            <antidote-lab-stage-selector></antidote-lab-stage-selector>        
-        </div> 
+        ${unsafeHTML(converter.makeHtml(lessonDetailsRequest.data.GuideContents))}
+        <antidote-lab-stage-selector></antidote-lab-stage-selector>
       `;
     }
   }
 
   useSyncronizedScrolling.apply(this);
-
-  return html`
+  return lessonRequest.completed && lessonDetailsRequest.completed ? html`
+    <div>
+      <h2>${lessonRequest.data.Name}</h2>
+      <h3 style="margin-top: 0px;">Chapter ${lessonStage+1} - ${lessonRequest.data.Stages[lessonStage].Description}</h3>
+      ${lessonRequest.data.Authors && lessonRequest.data.Authors.length > 0 ? html`<p>
+        ${lessonRequest.data.Authors.length > 1 ? l8n('lab.author.plural.label') : l8n('lab.author.singular.label')}: ${lessonRequest.data.Authors.map((author, i) => html`<a target="_blank" href="${author.Link}">${author.Name}</a>${(i>=lessonRequest.data.Authors.length-1) ? '' : ', '}`)}
+      </p>`: ''}
+      <hr />
     <link rel="stylesheet" href=${getComponentStyleSheetURL(this)} />
     ${guideContent}
-  `;
+    </div>
+  ` : '';
 }
 
 customElements.define('antidote-lab-guide', component(LabGuide));
