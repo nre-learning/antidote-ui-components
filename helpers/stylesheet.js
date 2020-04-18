827ccee34467baf1a8082fb97b49a4b78f891fbe
abathur <abathur@networkrelibility.engineering>
2020-04-18 05:42:20 +0000

    Preparing for release v0.6.0
    Signed-off-by: abathur <abathur@networkrelibility.engineering>

--- helpers/stylesheet.js
+++ helpers/stylesheet.js
@@ -1,5 +1,5 @@
 export default function getComponentStyleSheetURL(component) {
   return component.getAttribute('stylesheet')
     || window.antidoteStyleSheet
-    || "https://cdn.jsdelivr.net/gh/nre-learning/nre-styles@latest/dist/styles.css"
+    || "https://cdn.jsdelivr.net/gh/nre-learning/nre-styles@0.6.0/dist/styles.css"
 }
