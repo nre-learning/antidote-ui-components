1429da72a1dece78c51045127ff0b15ed600df65
Matt Oswalt <matt@oswalt.dev>
2020-04-22 09:52:03 -0700

    Clear cookies from all paths
    Signed-off-by: Matt Oswalt <matt@oswalt.dev>

--- helpers/use-fetch.js
+++ helpers/use-fetch.js
@@ -81,6 +81,8 @@ async function getSessionId(clearFirst) {
 
   if (clearFirst) {
     document.cookie = 'nreLabsSession=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    document.cookie = 'nreLabsSession=; expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/labs;';
+    document.cookie = 'nreLabsSession=; expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/;';
   }
 
   var sessionId = document.cookie.replace(/(?:(?:^|.*;\s*)nreLabsSession\s*\=\s*([^;]*).*$)|^.*$/, "$1");
